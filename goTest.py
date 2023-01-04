351e33d33aa71620f4c0c600da0ea62c63fe9ba8
xudan <875126243@qq.com>
2023-01-04 17:53:12 +0800

    fix update

---
+++ goTest.py
@@ -0,0 +1,102 @@
+from selenium import webdriver
+# import json
+from bs4 import BeautifulSoup
+import time
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import csv
+import numpy as np
+
+header = ['lableName', 'typeName', "url"]
+# header = ['name','age']
+data = [{'lableName':'suliang','typeName':'21'},
+        {'lableName':'xiaoming','typeName':'22'},
+        {'lableName':'xiaohu','typeName':'25'}]
+
+csvData = []
+
+# print(len(data))
+# exit()
+
+s = Service(r"E:/webroot/etherscan-labelcloud-spider/chromedriver.exe")
+
+options = Options()
+# options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--window-size=1920,1080')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--allow-running-insecure-content')
+options.add_argument('--disable-extentions')
+# options.add_argument("--proxy-server='direct://'")
+# options.add_argument('--proxy-bypass-list=*')
+options.add_argument('--start-maximized')
+options.add_argument('--disable-gpu')
+options.add_argument('--no--sandbox')
+options.add_argument('--log-level=3')
+
+driver = webdriver.Chrome(service=s,options=options)
+
+def isAccounts(accounts):
+   # print("accounts:"+ accounts )
+   # print(accounts.find("Accounts"))
+   if(accounts.find("Accounts") == 0):
+      return True
+   else:
+      return False
+
+def getData():
+      # print("abc")
+      trRows = driver.find_elements(By.CSS_SELECTOR, 'tbody > tr')
+      for elm in trRows:
+            tdRow = elm.find_elements(By.TAG_NAME, 'td')
+            # print(len(tdRow))
+            if len(tdRow) == 4:
+               address = tdRow[0].find_element(By.TAG_NAME, "a").get_attribute('text')        
+               addressDetailsLink = tdRow[0].find_element(By.TAG_NAME, "a").get_attribute('href')
+               nameTag = tdRow[1].text
+               balance = tdRow[2].text
+               txnCount = tdRow[3].text
+               print("============")
+               print("address: " +address+ " addressDetailsLink: " +addressDetailsLink+ "nameTag: " + nameTag + " balance" + balance + "txnCount" + txnCount )
+               print("============")
+
+
+try:
+   # driver.get('https://cn.etherscan.com/accounts/label/0x-protocol-ecosystem?size=100')
+   # driver.get('https://cn.etherscan.com/accounts/label/balancer?subcatid=1&size=5000')
+   driver.get('https://cn.etherscan.com/accounts/label/0x-protocol?subcatid=1&size=5000')
+   salary = input('请输入1：') 
+   print('run...' + salary )
+   time.sleep(3)
+   driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.END)
+   time.sleep(2)
+   rows = driver.find_elements(By.CSS_SELECTOR, 'ul.nav.nav-custom.nav-borderless.nav_tabs > li')
+   if (len(rows) > 0):
+   #  for elm in rows:
+      print("执行获取多tab数据")
+      for i in range(len(rows)):
+         if(i == 0):
+            print("走到第一步...")
+            getData()
+         elif(i == 1):
+            print("走到第二步...")
+            driver.get('https://cn.etherscan.com/accounts/label/0x-protocol?subcatid=3-0&size=5000')
+            time.sleep(5)
+            getData()
+         elif(i == 2):
+            print("走到第三步...")
+            driver.get('https://cn.etherscan.com/accounts/label/0x-protocol?subcatid=2&size=5000&')
+            time.sleep(5)
+            getData()
+      print(1111)
+      driver.quit()
+   else:
+      print("执行获取无tab数据")
+      getData()
+finally:
+   driver.quit()
+print("dene")
+
--- test.py
+++ test.py
@@ -1,24 +1,14 @@
 from selenium import webdriver
 # import json
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
+# import sys
 import time
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-import csv
-import numpy as np
-
-header = ['lableName', 'typeName', "url"]
-# header = ['name','age']
-data = [{'lableName':'suliang','typeName':'21'},
-        {'lableName':'xiaoming','typeName':'22'},
-        {'lableName':'xiaohu','typeName':'25'}]
-
-csvData = []
-
-# print(len(data))
-# exit()
+# import csv
+# import numpy as np
 
 s = Service(r"E:/webroot/etherscan-labelcloud-spider/chromedriver.exe")
 
@@ -47,32 +37,35 @@ def isAccounts(accounts):
 
 driver = webdriver.Chrome(service=s,options=options)
 try:
-   driver.get('https://cn.etherscan.com/labelcloud')
+#    driver.get('https://cn.etherscan.com/accounts/label/balancer?subcatid=1&size=100000&start=0&col=1&order=asc')
+   driver.get('https://cn.etherscan.com/accounts/label/0x-protocol-ecosystem?subcatid=undefined&size=100&start=0&col=1&order=asc')
+#    salary = input('请输入1：') 
+#    print('run...' + salary )
    time.sleep(3)
    driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.END)
    time.sleep(2)
-   rows = driver.find_elements(By.CSS_SELECTOR, 'div.row.mb-3 > div')
-   print("============")
-   for elm in rows:
-      lableName = elm.find_element(By.TAG_NAME, "button").__getattribute__('text')
-      menu = elm.find_elements(By.CSS_SELECTOR, 'div.dropdown-menu > a')
-      for aElm in menu:
-         menuName = aElm.get_attribute("textContent")
-         menuLink = aElm.get_attribute("href")
-         if(isAccounts(menuName)):
-            a = np.array({"lableName":lableName,"typeName": menuName, "url":menuLink})
-            c = np.append(csvData, a)
-            csvData = c
-   print("============")
-   print(len(csvData))
-   print(csvData)
+   rows = driver.find_elements(By.CSS_SELECTOR, 'ul.nav.nav-custom.nav-borderless.nav_tabs > li')
+   if (len(rows) > 0):
+      print("执行获取多tab数据")
+    #   获取 TAB 名称数量以及 url
+   else:
+      trRows = driver.find_elements(By.CSS_SELECTOR, 'tbody > tr')
+      for trDoc in trRows:
+        tdRow = trDoc.find_elements(By.CSS_SELECTOR, 'td')
 
-   
+      #   for tdDoc in tdRow:
+      #       str =  tdDoc.text
+      #       str1 =  tdDoc.find_elements(By.CSS_SELECTOR, 'a').__getattribute__("href")
+        # address = tdRow[0].text
+        # link = tdRow[0].find_elements(By.CSS_SELECTOR, 'a').__getattribute__("inerHTML")
+        # print("抓取没有tab 数据")
+        # print("address:" + address )
+            # print(str1)
 
-   with open ('information.csv','w',encoding='utf-8',newline='') as fp:
-      writer =csv.DictWriter(fp,header)
-      writer.writeheader()
-      writer.writerows(csvData)
+   print("============")
+   print(len(rows))
+   print("============")
+
    driver.quit()
 finally:
    driver.quit()
